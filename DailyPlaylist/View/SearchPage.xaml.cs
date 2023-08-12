0331184bea1e4e70a061cf761b58ca070e48044d
IvanovBass <xboulite020@hotmail.com>
2023-08-12 04:03:14 +0200

    on y est presque pour la SearchPage

--- DailyPlaylist/View/SearchPage.xaml.cs
+++ DailyPlaylist/View/SearchPage.xaml.cs
@@ -5,5 +5,6 @@ public partial class SearchPage : ContentPage
 	public SearchPage()
 	{
 		InitializeComponent();
+
 	}
 }
--- DailyPlaylist/ViewModel/SearchViewModel.cs
+++ DailyPlaylist/ViewModel/SearchViewModel.cs
@@ -1,12 +1,48 @@
-﻿using Newtonsoft.Json;
+﻿using MediaManager;
+using Newtonsoft.Json;
+using System.Windows.Input;
+using DailyPlaylist.View;
 
 namespace DailyPlaylist.ViewModel
 {
     public class SearchViewModel : BaseViewModel
     {
 
-        private string _searchQuery;
+        private string _albumCover = "music_notes2.png";  
+        public string AlbumCover
+        {
+            get { return _albumCover; }
+            set
+            {
+                _albumCover = value;
+                OnPropertyChanged(nameof(AlbumCover));
+            }
+        }
 
+        private string _artistName = "Artist";
+        public string ArtistName
+        {
+            get { return _artistName; }
+            set
+            {
+                _artistName = value;
+                OnPropertyChanged(nameof(ArtistName));
+            }
+        }
+
+        private string _trackName = "Song";
+        public string TrackName
+        {
+            get { return _trackName; }
+            set
+            {
+                _trackName = value;
+                OnPropertyChanged(nameof(TrackName));
+            }
+        }
+
+
+        private string _searchQuery;
 
         private ObservableCollection<Track> _searchResults;
 
@@ -61,15 +97,58 @@ namespace DailyPlaylist.ViewModel
                 // To notify the user with a message ...
             }
         }
-    }
-    // add a progress bar , aloading handler  or text to inform the user of the search
-    // You'll need a 'SearchData' class to help with deserialization, which may look something like this:
-    public class SearchData
-    {
-        public List<Track> Data { get; set; }
-        //... other properties you may want to capture.
-    }
+        public class SearchData
+        {
+            public List<Track> Data { get; set; }
+            //... other properties you may want to capture.
+            // add a progress bar , a loading widget or a text to inform the user of the search
+        }
 
+        // ...
+
+        public ICommand GetTrackCommand => new Command<Track>(async (track) => await GetTrackDetailsAsync(track));
+
+        private async Task GetTrackDetailsAsync(Track selectedTrack)
+        {
+            try
+            {
+                string apiUrl = $"https://api.deezer.com/track/{selectedTrack.Id}";
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetStringAsync(apiUrl);
+                    Track detailedTrack = JsonConvert.DeserializeObject<Track>(response);
+
+                    // Here, you can update any UI bindings to display the detailed track information.
+                    // For example, if you have properties for AlbumCover, TrackName, ArtistName, etc. in your ViewModel:
+                    AlbumCover = detailedTrack.Album.Cover;
+                    TrackName = detailedTrack.Title;
+                    ArtistName = detailedTrack.Artist.Name;
+
+                    if (!string.IsNullOrEmpty(detailedTrack.Preview))
+                    {
+                        await PlayTrackAsync(detailedTrack.Preview);
+                    }
+                    else
+                    {
+                        // Handle case where preview URL is null or empty.
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error fetching track details: {ex.Message}");
+            }
+        }
+
+        private async Task PlayTrackAsync(string previewUrl)
+        {
+            await CrossMediaManager.Current.Play(previewUrl);
+            Debug.WriteLine("OK je suis rentré dedans");
+            Debug.WriteLine(previewUrl);
+            // Here you can add any additional logic if required after starting the playback.
+        }
+
+    }
 }
 
 // https://api.deezer.com/search?q={your_query}
