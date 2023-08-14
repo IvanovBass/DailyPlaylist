64204a617497ea0b5fcd8376fe041606b1508d56
IvanovBass <xboulite020@hotmail.com>
2023-08-14 05:04:16 +0200

    Ok separation du service MediaPlayer

--- DailyPlaylist/ViewModel/SearchViewModel.cs
+++ DailyPlaylist/ViewModel/SearchViewModel.cs
@@ -1,14 +1,17 @@
-﻿namespace DailyPlaylist.ViewModel
+﻿using DailyPlaylist.Services;
+
+namespace DailyPlaylist.ViewModel
 {
     public class SearchViewModel : BaseViewModel
     {
         private ObservableCollection<Track> _searchResults;
+        private int _preStoredIndex; 
+        private MediaPlayerService _mediaPlayerService;
         private string _searchQuery;
         private Track _selectedTrack;
         private string _trackName = "Song";
         private string _artistName = "Artist";
         private string _albumCover = "music_notes2.png";
-        private string _trackPreview;
         private readonly HttpClient _httpClient;
         private bool _isLoading;
 
@@ -46,12 +49,6 @@
             set => SetProperty(ref _albumCover, string.IsNullOrEmpty(value) ? "music_notes2.png" : value);
         }
 
-        public string TrackPreview
-        {
-            get => _trackPreview;
-            set => SetProperty(ref _trackPreview, value);
-        }
-
         public Track SelectedTrack
         {
             get => _selectedTrack;
@@ -60,10 +57,12 @@
                 SetProperty(ref _selectedTrack, value);
                 if (value != null)
                 {
+                    int selectedIndex = SearchResults.IndexOf(value);
+                    _preStoredIndex = selectedIndex;
+
                     AlbumCover = value.Album?.CoverMedium;
                     TrackName = value.Title;
                     ArtistName = value.Artist?.Name;
-                    TrackPreview = value.Preview;
                 }
             }
         }
@@ -74,10 +73,10 @@
             set => SetProperty(ref _isLoading, value);
         }
 
-
-
         public ICommand PlayPauseCommand { get; }
         public ICommand PlayFromCollectionViewCommand { get; }
+        public ICommand NextCommand { get; }
+        public ICommand PreviousCommand { get; }  
         public ICommand ItemSelectedCommand { get; }
 
         public ICommand SearchCommand { get; }
@@ -90,63 +89,28 @@
             SearchCommand = new Command(PerformSearch);
             SearchResults = new ObservableCollection<Track>();
 
-            PlayPauseCommand = new Command(async () =>
+            PlayPauseCommand = new Command(async track =>
             {
-                var currentMediaItem = CrossMediaManager.Current.Queue.Current;
-
-                // Will check if a track is currently playing and if it's the same as the selected track !
-                if (CrossMediaManager.Current.IsPlaying() && currentMediaItem != null && currentMediaItem.MediaUri == SelectedTrack.Preview)
-                {
-                    await CrossMediaManager.Current.Pause();
-                }
-                else
-                {
-                    // Stops the current track (if a track is currently playing)
-                    if (currentMediaItem != null)
-                    {
-                        await CrossMediaManager.Current.Stop();
-                    }
-
-                    // Play the selected track
-                    if (!string.IsNullOrEmpty(TrackPreview))
-                    {
-                        try
-                        {
-                            await CrossMediaManager.Current.Play(TrackPreview);
-                        }
-                        catch (Exception ex)
-                        {
-                            Debug.WriteLine($"Error playing track: {ex.Message}");
-                            await ShowSnackBarAsync("Unable to play track", "Dismiss", () => { });
-                        }
-                    }
-                    else
-                    {
-                        await ShowSnackBarAsync("Preview not available", "Dismiss", () => { });
-                    }
-                }
+                await _mediaPlayerService.PlayPauseTaskAsync(_preStoredIndex);
+                Debug.WriteLine("le _preStoredIndex est : " + _preStoredIndex.ToString());
             });
-
 
             PlayFromCollectionViewCommand = new Command<Track>(async track =>
             {
                 SelectedTrack = track;
-                if (string.IsNullOrEmpty(track.Preview))
-                {
-                    await ShowSnackBarAsync("Preview not available", "Dismiss", () => { });
-                    return;
-                }
-                try
-                {
-                    await CrossMediaManager.Current.Play(track.Preview);
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine($"Error playing track: {ex.Message}");
-                    await ShowSnackBarAsync("Unable to play track", "Dismiss", () => { });
-                }
+                await _mediaPlayerService.PlayPauseTaskAsync(_preStoredIndex);
             });
 
+
+            NextCommand = new Command<Track>(async track =>
+            {
+                SelectedTrack = await _mediaPlayerService.PlayNextAsync();
+            });
+
+            PreviousCommand = new Command<Track>(async track =>
+            {
+                SelectedTrack = await _mediaPlayerService.PlayPreviousAsync();
+            });
 
             ItemSelectedCommand = new Command<Track>(track =>
             {
@@ -167,6 +131,7 @@
                 var jsonResponse = await _httpClient.GetStringAsync($"https://api.deezer.com/search/track?q={SearchQuery}&limit=30");
                 var searchData = JsonConvert.DeserializeObject<SearchData>(jsonResponse);
                 SearchResults = new ObservableCollection<Track>(searchData.Data);
+                _mediaPlayerService = new MediaPlayerService(searchData.Data, -1);
             }
             catch (Exception ex)
             {
@@ -186,28 +151,6 @@
             //... other properties or objects to catch eventually
             // add a progress bar , a loading widget or a text to inform the user of the search
         }
-
-        public async Task ShowSnackBarAsync(string message, string actionText, Action action, int durationInSeconds = 3)
-        {
-            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-
-            var snackbarOptions = new SnackbarOptions
-            {
-                BackgroundColor = Colors.DarkSlateBlue,
-                TextColor = Colors.White,
-                ActionButtonTextColor = Colors.Orange,
-                CornerRadius = new CornerRadius(10),
-                
-                Font = Microsoft.Maui.Font.SystemFontOfSize(13),
-                ActionButtonFont = Microsoft.Maui.Font.SystemFontOfSize(13),
-                CharacterSpacing = 0.1
-            };
-
-            var snackbar = Snackbar.Make(message, action, actionText, TimeSpan.FromSeconds(durationInSeconds), snackbarOptions);
-
-            await snackbar.Show(cancellationTokenSource.Token);
-        }
-
 
     }
 }
