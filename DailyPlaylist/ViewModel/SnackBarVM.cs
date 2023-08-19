3fc7f067088bd7fae786cb22f1982bcb255031bf
IvanovBass <xboulite020@hotmail.com>
2023-08-19 22:34:36 +0200

    we go forward

--- DailyPlaylist/ViewModel/SnackBarVM.cs
+++ DailyPlaylist/ViewModel/SnackBarVM.cs
@@ -13,9 +13,30 @@
                 ActionButtonTextColor = Colors.Orange,
                 CornerRadius = new CornerRadius(10),
 
-                Font = Microsoft.Maui.Font.SystemFontOfSize(14),
-                ActionButtonFont = Microsoft.Maui.Font.SystemFontOfSize(14),
+                Font = Microsoft.Maui.Font.SystemFontOfSize(15),
+                ActionButtonFont = Microsoft.Maui.Font.SystemFontOfSize(15),
                 CharacterSpacing = 0
+            };
+
+            var snackbar = Snackbar.Make(message, action, actionText, TimeSpan.FromSeconds(durationInSeconds), snackbarOptions);
+
+            await snackbar.Show(cancellationTokenSource.Token);
+        }
+
+        public static async Task ShowSnackBarShortAsync(string message, string actionText, Action action, int durationInSeconds = 2)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            var snackbarOptions = new SnackbarOptions
+            {
+                BackgroundColor = Colors.DarkSlateBlue,
+                TextColor = Colors.White,
+                ActionButtonTextColor = Colors.Orange,
+                CornerRadius = new CornerRadius(10),
+
+                Font = Microsoft.Maui.Font.SystemFontOfSize(16),
+                ActionButtonFont = Microsoft.Maui.Font.SystemFontOfSize(16),
+                CharacterSpacing = 0.1
             };
 
             var snackbar = Snackbar.Make(message, action, actionText, TimeSpan.FromSeconds(durationInSeconds), snackbarOptions);
